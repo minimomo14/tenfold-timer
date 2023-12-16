e87bbae202be16aee8d22147d4356aab7506cef0
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2023-12-15 20:55:46 -0500

    trying to make countDown to work still in progress

--- script.js
+++ script.js
@@ -1,107 +1,115 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const confContainer = document.getElementById('app');
+    const beforeBeepAudio = document.getElementById('beforeBeep');
+    const endBeepAudio = document.getElementById('endBeep');
 
+    const intervalInput = document.getElementById('intervalTime');
+    const breakInput = document.getElementById('breakTime');
+    const startButton = document.getElementById('start');
+    const pauseButton = document.getElementById('pause');
+    const resetButton = document.getElementById('reset');
+    const statusHeader = document.getElementById('status');
+    const secondsSpan = document.getElementById('sec');
 
-// Add function to update current date and time
-function updateCurrentDateTime() {
-    const currentDate =
-        document.getElementById('current-date');
-    const currentTime =
-        document.getElementById('current-time');
+    const currentDate = document.getElementById('current-date');
+    const currentTime = document.getElementById('current-time');
 
-    // For the current date and time
-    const now = new Date();
-    const optionsDate = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
-    const optionsTime = { hour: 'numeric', minute: 'numeric', hour12: true };
+    function updateCurrentDateTime() {
+        const now = new Date();
+        const optionsDate = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
+        const optionsTime = { hour: 'numeric', minute: 'numeric', hour12: true };
 
-    const formattedDate = now.toLocaleString('en-US', optionsDate);
-    const formattedTime = now.toLocaleString('en-US', optionsTime);
+        const formattedDate = now.toLocaleString('en-US', optionsDate);
+        const formattedTime = now.toLocaleString('en-US', optionsTime);
 
-    currentDate.textContent = formattedDate;
-    currentTime.textContent = formattedTime; setTimeout(updateCurrentDateTime, 1000)
+        currentDate.textContent = formattedDate;
+        currentTime.textContent = formattedTime;
+        setTimeout(updateCurrentDateTime, 1000);
 
-    //check if date and time is working
-    //console.log(formattedDate)
-    //console.log( "clock ticking",formattedTime)
-    console.log(currentDate.textContent, currentTime.textContent)
-    console.log("Local timer is running here!")
-}
-
-
-
-
-window.onload = function() {
-
-    var seconds = 20;
-    var rest = true;
-    var interval;
-  
-    var intervalTime = 20;
-    var breakTime = 10;
-  
-    var settingsButton = document.getElementById("settings");
-    var intervalInput = document.getElementById("intervalTime");
-    var breakInput = document.getElementById("breakTime");
-  
-    var startButton = document.getElementById("start");
-    var pauseButton = document.getElementById("pause");
-    var resetButton = document.getElementById("reset");
-  
-    var statusHeader = document.getElementById("status");
-    var secondsSpan = document.getElementById("sec");
-  
-    settingsButton.onclick = function() {
-      intervalTime = Math.floor(intervalInput.value * 1);
-      breakTime = Math.floor(breakInput.value * 1);
-      reset();
+        console.log(currentDate.textContent, currentTime.textContent);
+        console.log("Local timer is running here!");
     }
-  
-    startButton.onclick = function() {
-      rest = false;
-      changeToGo();
-      interval = setInterval(countdownSeconds, 1000);
+
+    function validateNumericInput(element) {
+        const val = element.value;
+        if (isNaN(val) || val == 0 || val == '0') {
+            element.value = '1';
+        }
     }
-  
-    resetButton.onclick = function() {
-      reset();
-    }
-  
-    function reset() {
-      clearInterval(interval);
-      seconds = intervalTime;
-      secondsSpan.innerText = seconds;
-      rest = true;
-      changeToRest();
-    }
-  
-    pauseButton.onclick = function() {
-      clearInterval(interval);
-    }
-  
-    function countdownSeconds() {
-      seconds -= 1;
-      secondsSpan.innerText = seconds;
-      checkForStateChange();
-    }
-  
-    function checkForStateChange() {
-      if (seconds == 0 && rest == false) {
-        seconds = breakTime + 1;
-        rest = true;
-        changeToRest();
-      } else if (seconds == 0 && rest == true) {
-        seconds = intervalTime + 1;
+
+    function startTimer() {
         rest = false;
         changeToGo();
-      }
+        interval = setInterval(countdownSeconds, 1000);
     }
-  
+
+    function resetTimer() {
+        clearInterval(interval);
+        seconds = intervalTime;
+        secondsSpan.innerText = seconds;
+        rest = true;
+        changeToRest();
+    }
+
+    function pauseTimer() {
+        clearInterval(interval);
+    }
+
+    function updateTimerInputs() {
+        intervalTime = Math.floor(intervalInput.value * 1);
+        breakTime = Math.floor(breakInput.value * 1);
+        resetTimer();
+    }
+
+    function countdownSeconds() {
+        seconds -= 1;
+        secondsSpan.innerText = seconds;
+        checkForStateChange();
+    }
+
+    function checkForStateChange() {
+        if (seconds === 0 && !rest) {
+            seconds = breakTime + 1;
+            rest = true;
+            changeToRest();
+        } else if (seconds === 0 && rest) {
+            seconds = intervalTime + 1;
+            rest = false;
+            changeToGo();
+        }
+    }
+
     function changeToRest() {
-      $("body").css("background", "cyan");
-      statusHeader.innerText = "Rest";
+        document.body.style.background = 'cyan';
+        statusHeader.innerText = 'Rest';
     }
-  
+
     function changeToGo() {
-      $("body").css("background", "pink");
-      statusHeader.innerText = "Go!";
+        document.body.style.background = 'pink';
+        statusHeader.innerText = 'Go!';
     }
-  
-  }
+
+    intervalInput.addEventListener('keyup', function () {
+        validateNumericInput(intervalInput);
+    });
+
+    breakInput.addEventListener('keyup', function () {
+        validateNumericInput(breakInput);
+    });
+
+    startButton.addEventListener('click', function () {
+        updateTimerInputs();
+        startTimer();
+    });
+
+    resetButton.addEventListener('click', function () {
+        updateTimerInputs();
+        resetTimer();
+    });
+
+    pauseButton.addEventListener('click', function () {
+        pauseTimer();
+    });
+
+    updateCurrentDateTime();
+});
